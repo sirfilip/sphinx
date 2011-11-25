47c05ece3dacc9944f4e171eb7399e0ce4173f8e
Filip Kostovski <github.sirfilip@gmail.com>
2011-11-25 02:58:43 +0100

    Finished method delegation

--- config/sphinxsearch.php
+++ config/sphinxsearch.php
@@ -1,4 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['host'] = 'localhost';
-$config['port'] = 9313;
+$config['server']          = array('localhost', 9312);
+$config['connect_timeout'] = 1000;
+$config['array_result']    = false;
+$config['max_query_time']  = 1000;
